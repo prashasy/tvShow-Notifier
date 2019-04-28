c8ed971ec341548eb2c48b8be932475314d6c730
prashasy <prashasyashok@gmail.com>
2019-04-28 11:06:59 +0530

    added error handling

--- update_database.py
+++ update_database.py
@@ -4,10 +4,15 @@ import sqlite3 as sql
 def run_update():
 	con=sql.connect("user_data.db")
 	cur=con.cursor()
-	res=cur.execute("select name from tv_series")
-	m=""
-	for item in res:
-		m+="TV Series: "+item +"\n" +"Status: "+tv.imdb_data(item)+"\n\n"
-		result=cur.execute("insert into tv_series(updates) values(?)",(m,))
-
+	try:
+		res=cur.execute("select name from tv_series")
+		m=""
+		for item in res:
+			m+="TV Series: "+item +"\n" +"Status: "+tv.imdb_data(item)+"\n\n"
+			try:
+				result=cur.execute("insert into tv_series(updates) values(?)",(m,))
+			except:
+				print("Error inserting data for {}".format("item"))
+	except:
+		print("Error finding names of series")
 run_update()
